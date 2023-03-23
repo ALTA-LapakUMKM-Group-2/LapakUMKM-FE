8bae34498bba7c8e8bcdde8e6ccfe5ee54a3be7d
Agung Cahya <agungcahya122@gmail.com>
2023-03-23 20:31:52 +0700

    feat: integrasi feedback

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import HomeFilter from "./pages/HomeFilterPage";
 import ListProduct from "./pages/ListProduct";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
-import HomeFilter from "./pages/HomeFilterPage";
 import Toko from "./pages/Toko";
 import Cart from "./pages/Cart";
 import Payment from "./pages/Payment";
 import Detail from "./pages/Detail";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import NotifikasiCoba from "./pages/NotifikasiCoba";
 
 
 
@@ -24,16 +24,17 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/home" element={<Home/>}/>
-          <Route path="/detail/:id" element={<Detail/>}/>
-          <Route path="/payment" element={<Payment/>} />
-          <Route path="/cart" element={<Cart/>} />
-          <Route path="/home" element={<Home/>}/>       
-          <Route path="/home/:kategori" element={<HomeFilter/>}/>       
-          <Route path="/profile" element={<Profile/>} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/detail/:id" element={<Detail />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/home/:kategori" element={<HomeFilter />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="/listproduct" element={<ListProduct />} />
           <Route path="/historypembeli" element={<HistoryPembeli />} />
           <Route path="/toko" element={<Toko />} />
+          <Route path="/notif" element={<NotifikasiCoba />} />
         </Routes>
       </BrowserRouter>
     </>
